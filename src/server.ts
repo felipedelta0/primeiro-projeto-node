481ce601ae514e84d8b775fdd021c0fae2c70d95
Luis Felipe <felipedelta0@gmail.com>
2020-08-15 18:18:01 -0300

    Prettier configurado

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,7 @@ import routes from './routes'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello, World! 🌎' })
-})
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('🚀 Server Started on Port 3333')
