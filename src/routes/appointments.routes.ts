46e0055ee764adbfac35134185e0448551c89f74
Luis Felipe <felipedelta0@gmail.com>
2020-08-18 21:51:37 -0300

    finalizada acesso ao repositorio no service

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -16,6 +17,12 @@ appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = parseISO(date);
+
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
+
+  const appointment = createAppointment.execute({ date: parsedDate, provider });
 
   return response.json(appointment);
 });
